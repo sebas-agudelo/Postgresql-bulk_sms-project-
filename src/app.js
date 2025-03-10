fa0ddffd2315c3941f1a54306dbc5e0d2b853996
Sebastian Agudelo <sebastianagudelo95@hotmail.com>
2025-03-10 13:42:14 +0100

    Changed Prisma schema

--- src/app.js
+++ src/app.js
@@ -3,8 +3,6 @@ import dotenv from 'dotenv';
 import { PrismaClient } from "@prisma/client";
 import fs from 'fs'; 
 
-
-
 const app = express();
 app.use(express.json());
 
@@ -15,40 +13,41 @@ const prisma = new PrismaClient();
 const participants = fs.readFileSync('src/json/participants.json', 'utf-8');
 const participants_data = JSON.parse(participants).data;
 
-app.get('/payload', async (req, res) => {
-    const user = await prisma.bulk_sms_users.create({
-        data: {
-            profileName: "TestUser",
-            message: "Hi there",
-            scheduleDate: "25-03-07 12:00:00",
-            created: new Date()
-        }
-    })
+app.post('/payload', async (req, res) => {
+    participants_data.forEach(async (allData) => {
+        
+        const { profileName, message, scheduleDate, participants: participantList } = allData;
 
-    participants_data.forEach( async (participant ) => {
-       
-        await prisma.participants.create({
-            
+        const user = await prisma.bulk_sms_users.create({
             data: {
-                userId: user.id,
-                name: participant.name,
-                phone: participant.phone,
-                couponSend: 0,
-                sms_parts: "0",
-                sms_cost: "0",
-                sent_date_time: "",
-                sms_created_time: "",
-                sms_id: "",
+                profileName: profileName,
+                message: message,
+                scheduleDate: scheduleDate,
                 created: new Date()
-
             }
-        })
-        
-        
-    })
-})
+        });
+
+        participantList.forEach(async (participant) => {
+            await prisma.participants.create({
+                data: {
+                    userId: user.id, 
+                    name: participant.name,
+                    phone: participant.phone,
+                    couponSend: 0,
+                    sms_parts: "0", 
+                    sms_cost: "0", 
+                    sent_date_time: "",
+                    sms_created_time: "",
+                    sms_id: "",
+                    created: new Date()
+                }
+            });
+        });
+    });
+
+    res.status(200).json({ message: "Data successfully saved!" });
+});
 
 app.listen(process.env.PORT, () => {
-    console.log("Servern är igång i porten 3000");
-    
-})
+    console.log("Server running on port 3000");
+});
