86c64f482dda050faad4538217c079259338a81a
Sebastian Agudelo <sebastianagudelo95@hotmail.com>
2025-03-12 09:56:10 +0100

    .

--- src/app.js
+++ src/app.js
@@ -1,49 +1,77 @@
-import express from 'express';
-import dotenv from 'dotenv';
+import express from "express";
+import dotenv from "dotenv";
 import { PrismaClient } from "@prisma/client";
-import fs from 'fs'; 
-
-
+import { users_data } from "./json/user_data.js";
 
 const app = express();
 app.use(express.json());
 
 dotenv.config();
 
+const participants_data = users_data.data;
+
 const prisma = new PrismaClient();
 
-const participants = fs.readFileSync('src/json/participants.json', 'utf-8');
-const user_data = JSON.parse(participants)
-const participants_data = JSON.parse(participants).data;
+app.post("/payload", async (req, res) => {
+  let FailedData = [];
 
-app.post('/payload', async (req, res) => {
+  try {
     const user = await prisma.bulk_sms_users.create({
-        data: {
-            profileName: user_data.profileName,
-            message: user_data.message,
-            scheduleDate: user_data.scheduledTime,
-            created: new Date()
-        }
-    })
+      data: {
+        profileName: users_data.profileName,
+        message: users_data.message,
+        scheduleDate: users_data.scheduledTime,
+        created: new Date(),
+      },
+    });
 
-    participants_data.forEach( async (participant ) => {
-       
+
+    for (const participant of participants_data) {
+      try {
         await prisma.participants.create({
-            
-            data: {
-                userId: user.id,
-                name: participant.name,
-                phone: participant.phone,
-                created: new Date()
-
-            }
+          data: {
+            userId: user.id,
+            name: participant.name,
+            phone: participant.phone,
+            created: new Date(),
+          },
         })
-        
-        
-    })
-})
+      } catch (error) {
+        FailedData.push({
+          name: participant.name,
+          phone: participant.phone,
+        })
+      }
+    }
+
+    if (FailedData.length > 0) {
+      for (const failed_participants of FailedData) {
+        try {
+           await prisma.participants.create({
+          data: {
+            userId: user.id,
+            name: failed_participants.name,
+            phone: failed_participants.phone,
+            created: new Date(),
+          },
+        })
+        } catch (error) {
+          return res.status(400).json({
+            message: `Something went wrong with one or more participants from the account: ${users_data.profileName}`,
+            error: error,
+          });
+        }
+      }
+    }
+
+    return res
+      .status(200)
+      .json("The data has been inserted into the database.");
+  } catch (error) {
+    return res.status(500).json({ error: "Something went wrong" });
+  }
+});
 
 app.listen(process.env.PORT, () => {
-    console.log("Servern är igång i porten 3000");
-    
-})
+  console.log("Servern är igång i porten 3000");
+});
